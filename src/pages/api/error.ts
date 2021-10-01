4db2b58f46275d0e9ddfa66b7410559c24e3bbee
Chris Stavitsky <corridorr@gmail.com>
2021-10-01 13:31:04 -0700

    remove unreachable code

--- src/pages/api/error.ts
+++ src/pages/api/error.ts
@@ -1,18 +1,16 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import { withSentry } from "@sentry/nextjs";
+import { withSentry } from '@sentry/nextjs';
 import * as Sentry from '@sentry/nextjs';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     console.log('try api');
     console.log('*** Headers ***');
     console.log(req.headers);
-    throw new Error("API throw error test");
-    res.status(200).json({ name: "John Doe" });
-  }
-  catch (ex) {
-    console.log('caught exception')
-    Sentry.captureException(ex)
+    throw new Error('API throw error test');
+  } catch (ex) {
+    console.log('caught exception');
+    Sentry.captureException(ex);
     res.json({});
     res.status(500);
     res.end();
