028467e203d21dc4147e29988985a0c23f9263bd
Chris Stavitsky <corridorr@gmail.com>
2021-10-04 11:46:29 -0700

    update to target:experimental-serverless-trace

--- next.config.js
+++ next.config.js
@@ -33,7 +33,7 @@ const moduleExports = withBundleAnalyzer({
   future: {
     webpack5: true,
   },
-  target: 'serverless',
+  target: 'experimental-serverless-trace',
 });
 
 // Make sure adding Sentry options is the last code to run before exporting, to
