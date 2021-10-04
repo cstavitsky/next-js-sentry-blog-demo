d290be699d2346e79e5abab7d902027714ae3091
Chris Stavitsky <corridorr@gmail.com>
2021-10-04 11:16:37 -0700

    set target:serverless for netlify

--- next.config.js
+++ next.config.js
@@ -33,9 +33,9 @@ const moduleExports = withBundleAnalyzer({
   future: {
     webpack5: true,
   },
+  target: 'serverless',
 });
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
 module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
-
